4af2082b3fbbf5a62cc323989ee1fe4449415b69
Su Nguyen T <vnntsu@gmail.com>
2022-12-05 10:31:53 +0700

    [#6] Update test

--- test/crawler_web/features/home_page/view_home_page_test.exs
+++ test/crawler_web/features/home_page/view_home_page_test.exs
@@ -1,5 +1,5 @@
 defmodule CrawlerWeb.HomePage.ViewHomePageTest do
-  use CrawlerWeb.FeatureCase
+  use CrawlerWeb.FeatureCase, async: false
 
   feature "view home page", %{session: session} do
     visit(session, Routes.page_path(CrawlerWeb.Endpoint, :index))
