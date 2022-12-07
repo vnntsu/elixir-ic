e38b77abba53a5a6c2a6c71e0896e5b633b8587a
Su Nguyen T <vnntsu@gmail.com>
2022-12-07 21:47:08 +0700

    [#7] Format html files

--- lib/crawler_web/telemetry.ex
+++ lib/crawler_web/telemetry.ex
@@ -48,8 +48,7 @@ defmodule CrawlerWeb.Telemetry do
       ),
       summary("crawler.repo.query.idle_time",
         unit: {:native, :millisecond},
-        description:
-          "The time the connection spent waiting before being checked out for the query"
+        description: "The time the connection spent waiting before being checked out for the query"
       ),
 
       # VM Metrics
