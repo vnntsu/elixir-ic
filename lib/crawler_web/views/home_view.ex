f00c5247b44aaf355d0c6e36b1b2d38332822f87
Su Nguyen T <vnntsu@gmail.com>
2023-01-10 16:21:04 +0700

    [#19] Pretty table view

--- lib/crawler_web/views/home_view.ex
+++ lib/crawler_web/views/home_view.ex
@@ -1,3 +1,23 @@
 defmodule CrawlerWeb.HomeView do
   use CrawlerWeb, :view
+
+  def get_column_status(value) do
+    case value do
+      :new -> "text-info"
+      :in_progress -> "text-warning"
+      :failed -> "text-danger"
+      :completed -> "text-success"
+      _ -> "text-muted"
+    end
+  end
+
+  def get_status(value) do
+    case value do
+      :new -> "New"
+      :in_progress -> "Crawling"
+      :failed -> "Failed"
+      :completed -> "Completed"
+      _ -> "Undefined"
+    end
+  end
 end
