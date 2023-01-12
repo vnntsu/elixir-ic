e2b3fedf187e39859db833210730fb6f9f9215a1
Su Nguyen T <vnntsu@gmail.com>
2023-01-12 11:38:55 +0700

    [#19] Update test cases

--- test/crawler_web/views/home_view_test.exs
+++ test/crawler_web/views/home_view_test.exs
@@ -7,33 +7,33 @@ defmodule CrawlerWeb.HomeViewTest do
     test "given status new, returns text-info",
       do: assert(HomeView.get_column_status(:new) == "text-info")
 
-    test "given status in_progress, returns text-info",
+    test "given status in_progress, returns text-warning",
       do: assert(HomeView.get_column_status(:in_progress) == "text-warning")
 
-    test "given status completed, returns text-info",
+    test "given status completed, returns text-success",
       do: assert(HomeView.get_column_status(:completed) == "text-success")
 
-    test "given status failed, returns text-info",
+    test "given status failed, returns text-danger",
       do: assert(HomeView.get_column_status(:failed) == "text-danger")
 
-    test "given status undefined, returns text-info",
+    test "given status undefined, returns text-muted",
       do: assert(HomeView.get_column_status(:undefined) == "text-muted")
   end
 
   describe "get_status/2" do
-    test "given status new, returns text-info",
+    test "given status new, returns New",
       do: assert(HomeView.get_status(:new) == gettext("New"))
 
-    test "given status in_progress, returns text-info",
+    test "given status in_progress, returns Crawling",
       do: assert(HomeView.get_status(:in_progress) == gettext("Crawling"))
 
-    test "given status completed, returns text-info",
+    test "given status completed, returns Completed",
       do: assert(HomeView.get_status(:completed) == gettext("Completed"))
 
-    test "given status failed, returns text-info",
+    test "given status failed, returns Failed",
       do: assert(HomeView.get_status(:failed) == gettext("Failed"))
 
-    test "given status undefined, returns text-info",
+    test "given status undefined, returns Undefined",
       do: assert(HomeView.get_status(:undefined) == gettext("Undefined"))
   end
 end
