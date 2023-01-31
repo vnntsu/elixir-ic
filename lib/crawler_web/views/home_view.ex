d1f5e021f59450c5badf280ee1f87c24163e6720
Su Nguyen T <vnntsu@gmail.com>
2023-01-31 11:38:13 +0700

    [#20] Create detail ui

--- lib/crawler_web/views/home_view.ex
+++ lib/crawler_web/views/home_view.ex
@@ -1,23 +1,5 @@
 defmodule CrawlerWeb.HomeView do
   use CrawlerWeb, :view
 
-  def get_column_status(value) do
-    case value do
-      :new -> "text-info"
-      :in_progress -> "text-warning"
-      :failed -> "text-danger"
-      :completed -> "text-success"
-      _ -> "text-muted"
-    end
-  end
-
-  def get_status(value) do
-    case value do
-      :new -> gettext("New")
-      :in_progress -> gettext("Crawling")
-      :failed -> gettext("Failed")
-      :completed -> gettext("Completed")
-      _ -> gettext("Undefined")
-    end
-  end
+  import CrawlerWeb.ViewHelpers
 end
---
+++ lib/crawler_web/views/view_helpers.ex
@@ -0,0 +1,25 @@
+defmodule CrawlerWeb.ViewHelpers do
+  import CrawlerWeb.Gettext
+
+  def is_completed(status), do: status == :completed
+
+  def get_status(value) do
+    case value do
+      :new -> gettext("New")
+      :in_progress -> gettext("Crawling")
+      :failed -> gettext("Failed")
+      :completed -> gettext("Completed")
+      _ -> gettext("Undefined")
+    end
+  end
+
+  def get_column_status(value) do
+    case value do
+      :new -> "text-info"
+      :in_progress -> "text-warning"
+      :failed -> "text-danger"
+      :completed -> "text-success"
+      _ -> "text-muted"
+    end
+  end
+end
