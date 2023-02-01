56823f6aaf1adc5562d46fa41622adeb7323a0cf
Su Nguyen T <vnntsu@gmail.com>
2023-02-01 18:00:56 +0700

    [#24] Create filter form

--- lib/crawler_web/views/home_view.ex
+++ lib/crawler_web/views/home_view.ex
@@ -2,4 +2,12 @@ defmodule CrawlerWeb.HomeView do
   use CrawlerWeb, :view
 
   import CrawlerWeb.ViewHelpers
+
+  def operation_selected?(selected_operator, operator) do
+    if selected_operator == operator do
+      "selected"
+    else
+      ""
+    end
+  end
 end
