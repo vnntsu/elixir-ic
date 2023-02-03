7d54dd988a196649f4db9af9700f0b4c99cb2071
Su Nguyen T <vnntsu@gmail.com>
2023-02-03 14:41:01 +0700

    [#23] Filter name

--- lib/crawler_web/controllers/home_controller.ex
+++ lib/crawler_web/controllers/home_controller.ex
@@ -6,7 +6,7 @@ defmodule CrawlerWeb.HomeController do
 
   def index(conn, params) do
     changeset = KeywordCSVFile.create_changeset(%KeywordCSVFile{})
-    keywords = Keywords.list_keywords(conn.assigns.current_user.id)
+    keywords = Keywords.list_keywords_by_filter_params(conn.assigns.current_user.id, params)
 
     render(conn, "index.html", keywords: keywords, changeset: changeset, params: params)
   end
